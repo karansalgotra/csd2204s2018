353fdf6e52749d834078e0063f3114627fa26550
Karan Salgotra <39058649+karansalgotra@users.noreply.github.com>
2018-05-09 11:36:52 -0400

    Add files via upload

---
+++ Day3LogKARANSALGOTA1.sql
@@ -0,0 +1,16 @@
+MariaDB [CSD2204S18]> tee C:\Users\738026\Downloads\Day3LogKARANSALGOTA1.sql 
+MariaDB [CSD2204S18]> tee C:\Users\738026\Downloads\Day3LogKARANSALGOTA1.sql ;
+ERROR: Unknown command '\U'.
+ERROR: Unknown command '\7'.
+ERROR: Unknown command '\D'.
+ERROR: Unknown command '\D'.
+MariaDB [CSD2204S18]> tee C:\Users\738026\Downloads\Day3LogKARANSALGOTA1.sql 
+MariaDB [CSD2204S18]> SHOW TABLES;
++----------------------+
+| Tables_in_csd2204s18 |
++----------------------+
+| customer             |
++----------------------+
+1 row in set (0.00 sec)
+
+MariaDB [CSD2204S18]> EXIT;
